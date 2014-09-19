ef741c3fc8816b064ad25b77f0c50683417f15b4
Steve Martinez <j.steve.martinez@gmail.com>
2014-09-19 12:06:34 -0700

    Finish user signup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 SampleApp::Application.initialize!
+# Speed up tests by lowering bcrypt's cost function.
+ActiveModel::SecurePassword.min_cost = true
