c8f06f5d3e88080103bb8c2d7fbee26cef102c80
Steve Martinez <j.steve.martinez@gmail.com>
2014-09-27 09:35:16 -0700

    added microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   before_save { self.email = email.downcase }
   before_create :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
@@ -9,7 +10,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }
   before_create :create_remember_token
-  
+
   def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
