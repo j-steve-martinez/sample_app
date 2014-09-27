4fbb3e5bac2a3488d4e0a2815a75fed4450cc817
Steve Martinez <j.steve.martinez@gmail.com>
2014-09-27 13:18:39 -0700

    Fix merge disaster

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -3,8 +3,6 @@ require 'spec_helper'
 describe "Authentication" do
 
   subject { page }
-  it { should_not have_link('Profile') }
-  it { should_not have_link('Settings') }
 
   describe "signin" do
     before { visit signin_path }
@@ -14,8 +12,6 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error') }
-      it { should_not have_link('Profile') }
-      it { should_not have_link('Settings') }
     end
 
     describe "with valid information" do
@@ -32,8 +28,6 @@ describe "Authentication" do
       describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
-        it { should_not have_link('Profile') }
-        it { should_not have_link('Settings') }
       end
     end
   end
@@ -77,35 +71,6 @@ describe "Authentication" do
         it { should have_title('Sign in') }
       end
 
-<<<<<<< HEAD
-      describe "when attempting to visit a protected page" do
-        before do
-          visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
-        end
-
-        describe "after signing in" do
-
-          it "should render the desired protected page" do
-            expect(page).to have_title('Edit user')
-          end
-
-          describe "when signing in again" do
-            before do
-              click_link "Sign out"
-              visit signin_path
-              fill_in "Email",    with: user.email
-              fill_in "Password", with: user.password
-              click_button "Sign in"
-            end
-
-            it "should render the default (profile) page" do
-              expect(page).to have_title(user.name)
-            end
-          end
-=======
       describe "in the Microposts controller" do
 
         describe "submitting to the create action" do
@@ -116,7 +81,6 @@ describe "Authentication" do
         describe "submitting to the destroy action" do
           before { delete micropost_path(FactoryGirl.create(:micropost)) }
           specify { expect(response).to redirect_to(signin_path) }
->>>>>>> user-microposts
         end
       end
     end
